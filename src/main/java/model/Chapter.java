ed1268072c13c01f8761982211773006d5f9367b
Sean Shookman <sms112788@gmail.com>
2016-07-03 10:37:02 -0500

    Updating Chapter Object

--- src/main/java/model/Chapter.java
+++ src/main/java/model/Chapter.java
@@ -8,19 +8,47 @@ package model;
  */
 public class Chapter {
 
+	private Integer id;
+	private Integer number;
 	private String title;
 	private String message;
 
-	public Chapter(String title, String message) {
+	public Chapter(Integer id, Integer number, String title, String message) {
+		this.id = id;
+		this.number = number;
 		this.title = title;
 		this.message = message;
 	}	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
 
 	public String getTitle() {
 		return title;
 	}
 	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public String getMessage() {
 		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
