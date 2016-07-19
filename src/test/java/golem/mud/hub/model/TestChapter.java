02caa310c4dd5b4121942350c82bb6f5589489c7
Sean Shookman <sms112788@gmail.com>
2016-07-18 22:51:59 -0500

    Mockito to the rescue!

--- src/test/java/golem/mud/hub/model/TestChapter.java
+++ src/test/java/golem/mud/hub/model/TestChapter.java
@@ -2,7 +2,10 @@ package golem.mud.hub.model;
 
 import java.sql.ResultSet;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TestChapter {
 
@@ -10,5 +13,23 @@ public class TestChapter {
 	public void testGetTable() {
 		Chapter chapter = new Chapter();
 		assertEquals("chapter", chapter.getTable());
+	}
+
+	@Test
+	public void testInstance() throws Exception {
+		ResultSet resultSet = mock(ResultSet.class);
+		when(resultSet.getInt("id")).thenReturn(1);
+		when(resultSet.getInt("prev_chapter_id")).thenReturn(5);
+		when(resultSet.getString("title")).thenReturn("TITLE");
+		when(resultSet.getString("body")).thenReturn("BODY");
+
+		Chapter chapterClass = new Chapter();
+		Chapter instance = chapterClass.instance(resultSet);
+
+		assertNotNull(instance);
+		assertEquals(Integer.valueOf(1), instance.getId());
+		assertEquals(Integer.valueOf(5), instance.getPrevChapterId());
+		assertEquals("TITLE", instance.getTitle());
+		assertEquals("BODY", instance.getBody());
 	}
 }
