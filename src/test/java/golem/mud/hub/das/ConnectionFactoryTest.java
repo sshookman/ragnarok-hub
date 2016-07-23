cb18fe084e7e542a393b35d157aa6937f6f28bc7
Sean Shookman <sms112788@gmail.com>
2016-07-22 22:40:27 -0500

    ConnectionFactory Test Case

---
+++ src/test/java/golem/mud/hub/das/ConnectionFactoryTest.java
@@ -0,0 +1,23 @@
+package golem.mud.hub.das;
+
+import org.junit.Test;
+import java.sql.Connection;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class ConnectionFactoryTest {
+
+	@Test
+	public void testGetConnection() throws Exception {
+		Connection game = ConnectionFactory.getConnection("library/Throwing_Shadows.sqlite");
+		assertNotNull(game);
+		
+		try {
+			ConnectionFactory.getConnection("not-a-library/NotAGame.sqlite");
+			fail("Exception Expected");
+		} catch (Exception exception) {
+			assertNotNull(exception);
+		}
+	}
+}
