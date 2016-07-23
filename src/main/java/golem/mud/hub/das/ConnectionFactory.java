d97047eae2c4b89bdd19df9718104f656e05ddd1
Sean Shookman <sms112788@gmail.com>
2016-07-22 22:05:55 -0500

    Creating ConnectionFactory

---
+++ src/main/java/golem/mud/hub/das/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package golem.mud.hub.das;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Logger;
+
+public class ConnectionFactory {
+	private static final Logger logger = Logger.getLogger(AbstractDataService.class.getName());
+
+	public static Connection getConnection(final String dbFile) throws Exception {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			return DriverManager.getConnection("jdbc:sqlite:" + dbFile);
+		} catch (Exception exception) {
+			logger.severe("Failed to Connect to Database: " + exception.getMessage());
+			throw exception;
+		}
+	}
+}
