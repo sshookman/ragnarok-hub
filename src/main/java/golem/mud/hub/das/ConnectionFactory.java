fa52c658f0518ff82b54473b4af07b4eac449f06
Sean Shookman <sms112788@gmail.com>
2016-07-22 22:26:04 -0500

    Fixing PMD error

--- src/main/java/golem/mud/hub/das/ConnectionFactory.java
+++ src/main/java/golem/mud/hub/das/ConnectionFactory.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.util.logging.Logger;
 
 public class ConnectionFactory {
-	private static final Logger logger = Logger.getLogger(AbstractDataService.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(AbstractDataService.class.getName());
 
 	public static Connection getConnection(final String dbFile) throws Exception {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			return DriverManager.getConnection("jdbc:sqlite:" + dbFile);
 		} catch (Exception exception) {
-			logger.severe("Failed to Connect to Database: " + exception.getMessage());
+			LOGGER.severe("Failed to Connect to Database: " + exception.getMessage());
 			throw exception;
 		}
 	}
