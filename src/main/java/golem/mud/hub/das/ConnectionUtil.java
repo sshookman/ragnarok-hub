3513a2d99f74e213a2815bdc89d5c098ded304e2
Sean Shookman <sms112788@gmail.com>
2016-07-24 12:15:32 -0500

    Adding init to ConnectionUtil

--- src/main/java/golem/mud/hub/das/ConnectionUtil.java
+++ src/main/java/golem/mud/hub/das/ConnectionUtil.java
@@ -2,10 +2,22 @@ package golem.mud.hub.das;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class ConnectionUtil {
-	private static final Logger LOGGER = Logger.getLogger(AbstractDataService.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(ConnectionUtil.class.getName());
+
+	private static void init(final Connection connection, final String query) {
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeQuery(query);
+			statement.close();
+		} catch (SQLException exception) {
+			LOGGER.severe("Failed to init: " + query);
+		}
+	}
 
 	public static Connection establishConnection(final String dbFile) throws Exception {
 		try {
@@ -19,11 +31,13 @@ public class ConnectionUtil {
 
 	public static Connection initGolemMudHub(final Connection connection) {
 
+		init(connection, "");
 		return connection;
 	}
 
 	public static Connection initGolem(final Connection connection) {
 
+		init(connection, "");
 		return connection;
 	}
 }
