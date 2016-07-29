bcd9fc9df0692208b6265e62df308d53871a1ebf
Sean Shookman <sms112788@gmail.com>
2016-07-28 21:25:58 -0500

    Adding ActionDO and Test Case

---
+++ src/main/java/golem/mud/hub/model/ActionDO.java
@@ -0,0 +1,94 @@
+package golem.mud.hub.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class ActionDO extends AbstractDO {
+
+	private Integer rowId;
+    private String type;
+    private Integer screenId;
+    private Integer golemId;
+    private String property;
+    private String value;
+
+	@Override
+	public Integer getRowId() {
+		return rowId;
+	}
+
+	@Override
+	public void setRowId(Integer rowId) {
+		this.rowId = rowId;
+	}
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getScreenId() {
+        return this.screenId;
+    }
+
+    public void setScreenId(Integer screenId) {
+        this.screenId = screenId;
+    }
+
+    public Integer getGolemId() {
+        return this.golemId;
+    }
+
+    public void setGolemId(Integer golemId) {
+        this.golemId = golemId;
+    }
+
+    public String getProperty() {
+        return this.property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+	@Override
+	public String getTable() {
+		return "action";
+	}
+
+	@Override
+	public ActionDO instance(ResultSet result) throws SQLException {
+		ActionDO instance = new ActionDO();
+		instance.setRowId(result.getInt("id"));
+		instance.setType(result.getString("type"));
+		instance.setScreenId(result.getInt("screen_id"));
+		instance.setGolemId(result.getInt("golem_id"));
+		instance.setProperty(result.getString("property"));
+		instance.setValue(result.getString("value"));
+		return instance;
+	}	
+
+	@Override
+	public Map<String, String> toMap() {
+		Map<String, String> dataMap = new HashMap<String, String>();
+		addNotNull("type", getType(), dataMap);
+		addNotNull("screen_id", getScreenId(), dataMap);
+		addNotNull("golem_id", getGolemId(), dataMap);
+		addNotNull("property", getProperty(), dataMap);
+		addNotNull("value", getValue(), dataMap);
+		return dataMap;
+	}
+}
---
+++ src/test/java/golem/mud/hub/model/ActionDOTest.java
@@ -0,0 +1,58 @@
+package golem.mud.hub.model;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import java.sql.ResultSet;
+import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ActionDOTest {
+
+	private static final ActionDO actionClass = new ActionDO();
+	private static ResultSet resultSet;
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		resultSet = mock(ResultSet.class);
+		when(resultSet.getInt("id")).thenReturn(1);
+		when(resultSet.getString("type")).thenReturn("TYPE");
+		when(resultSet.getInt("screen_id")).thenReturn(2);
+		when(resultSet.getInt("golem_id")).thenReturn(3);
+		when(resultSet.getString("property")).thenReturn("PROPERTY");
+		when(resultSet.getString("value")).thenReturn("VALUE");
+	}
+
+	@Test
+	public void testGetTable() throws Exception {
+		assertEquals("action", actionClass.getTable());
+	}
+
+	@Test
+	public void testInstance() throws Exception {
+		ActionDO instance = actionClass.instance(resultSet);
+
+		assertNotNull(instance);
+		assertEquals(Integer.valueOf(1), instance.getRowId());
+		assertEquals("TYPE", instance.getType());
+		assertEquals(Integer.valueOf(2), instance.getScreenId());
+		assertEquals(Integer.valueOf(3), instance.getGolemId());
+		assertEquals("PROPERTY", instance.getProperty());
+		assertEquals("VALUE", instance.getValue());
+	}
+
+	@Test
+	public void testToMap() throws Exception {
+		ActionDO action = actionClass.instance(resultSet);
+		Map<String, String> actionMap = action.toMap();
+
+		assertNotNull(actionMap);
+		assertEquals("\"TYPE\"", actionMap.get("type"));
+		assertEquals("2", actionMap.get("screen_id"));
+		assertEquals("3", actionMap.get("golem_id"));
+		assertEquals("\"PROPERTY\"", actionMap.get("property"));
+		assertEquals("\"VALUE\"", actionMap.get("value"));
+	}
+}
