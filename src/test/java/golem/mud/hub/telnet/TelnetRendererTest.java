ffda9dc209a70b0a62f1e5037011d05480f7e919
Sean Shookman <sms112788@gmail.com>
2016-08-07 22:37:41 -0500

    Finalizes test for TelnetRenderer
    Adds missing test for telnet renderer class to ensure reading
    and writing to socket connections is working properly.

--- src/test/java/golem/mud/hub/telnet/TelnetRendererTest.java
+++ src/test/java/golem/mud/hub/telnet/TelnetRendererTest.java
@@ -9,11 +9,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class TelnetRendererTest {
@@ -31,17 +33,35 @@ public class TelnetRendererTest {
         doNothing().when(writerMock).write(eq("valid"));
         doThrow(new RuntimeException("error")).when(writerMock).write(eq("invalid"));
 
+        when(readerMock.readLine()).thenReturn("valid").thenThrow(new IOException("error"));
+
         renderer = new TelnetRenderer(readerMock, writerMock);
     }
 
     @Test
     public void testWrite() {
         renderer.write("valid");
+        renderer.write("valid", 10);
 
         try {
             renderer.write("invalid");
             fail("Exception Expected");
         } catch (RuntimeException exception) {
+            assertNotNull(exception);
+            assertEquals("Expected Message to be 'error'", "error", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testRead() throws IOException {
+        String response = renderer.read();
+        assertNotNull(response);
+        assertEquals("Expected to read 'valid'", "valid", response);
+
+        try {
+            renderer.read();
+            fail("Exception Expected");
+        } catch (IOException exception) {
             assertNotNull(exception);
             assertEquals("Expected Message to be 'error'", "error", exception.getMessage());
         }
