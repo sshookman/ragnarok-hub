1d77b5cc4bf6a59379a7f85b5467bde6f1c8dbf4
Sean Shookman <sms112788@gmail.com>
2016-08-09 23:05:52 -0500

    Adds skeleton code for AbstractCommand Test Case
    Adds an empty test for AbstractCommand to be implemented
    in order to plan the design of the AbstractCommand.

---
+++ src/main/java/golem/mud/hub/command/AbstractCommand.java
@@ -0,0 +1,5 @@
+package golem.mud.hub.command;
+
+public abstract class AbstractCommand {
+
+}
---
+++ src/test/java/golem/mud/hub/command/AbstractCommandTest.java
@@ -0,0 +1,14 @@
+package golem.mud.hub.command;
+
+import org.junit.Test;
+
+public class AbstractCommandTest {
+
+    @Test
+    public void test() {
+    }
+
+    public class CommandImpl extends AbstractCommand {
+
+    }
+}
