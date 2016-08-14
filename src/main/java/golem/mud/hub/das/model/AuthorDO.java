03c4c79518a25885a18e1f65a6edf4ccfba47510
Sean Shookman <sms112788@gmail.com>
2016-08-14 12:54:36 -0500

    Adds AuthorDO and Test Case
    AuthorDO class represents an author of a Story available on
    the hub.

---
+++ src/main/java/golem/mud/hub/das/model/AuthorDO.java
@@ -0,0 +1,50 @@
+package golem.mud.hub.das.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class AuthorDO extends AbstractDO {
+
+	private Integer rowId;
+	private String username;
+
+	@Override
+	public Integer getRowId() {
+		return rowId;
+	}
+
+	@Override
+	public void setRowId(Integer rowId) {
+		this.rowId = rowId;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String getTable() {
+		return "author";
+	}
+
+	@Override
+	public AuthorDO instance(ResultSet result) throws SQLException {
+		AuthorDO instance = new AuthorDO();
+		instance.setRowId(result.getInt("id"));
+		instance.setUsername(result.getString("username"));
+		return instance;
+	}	
+
+	@Override
+	public Map<String, String> toMap() {
+		Map<String, String> dataMap = new HashMap<String, String>();
+		addNotNull("username", getUsername(), dataMap);
+		return dataMap;
+	}
+}
