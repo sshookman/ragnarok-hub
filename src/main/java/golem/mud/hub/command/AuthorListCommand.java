71323894e3bb86907c451ab825e82cb5f1e51a10
Sean Shookman <sms112788@gmail.com>
2016-08-14 20:20:55 -0500

    Adds execute logic to AuthorListCommand
    Lists the authors' usernames from the database.

--- src/main/java/golem/mud/hub/command/AuthorListCommand.java
+++ src/main/java/golem/mud/hub/command/AuthorListCommand.java
@@ -1,14 +1,22 @@
 package golem.mud.hub.command;
 
+import java.util.List;
+import java.util.HashMap;
+
 import golem.mud.hub.telnet.TelnetSession;
 import golem.mud.hub.telnet.TelnetRenderer;
+import golem.mud.hub.das.AuthorDataService;
+import golem.mud.hub.das.model.AuthorDO;
 
 public class AuthorListCommand extends AbstractAuthorCommand {
 
     private final TelnetRenderer renderer;
 
+    private final AuthorDataService authorDataService;
+
     public AuthorListCommand(final TelnetSession session) {
         this.renderer = session.getRenderer();
+        this.authorDataService = new AuthorDataService(session.getConnection());
     }
 
     public String getRegex() {
@@ -16,6 +24,11 @@ public class AuthorListCommand extends AbstractAuthorCommand {
     }
 
     public void execute() {
-        renderer.write("Listing all Authors");
+        renderer.write("All Authors:");
+        List<AuthorDO> authors = authorDataService.read(new HashMap<>());
+        for (AuthorDO author : authors) {
+            renderer.endl(1);
+            renderer.write(author.getUsername());
+        }
     }
 }
