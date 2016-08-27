67b89ce26e951b803f8202d7d33b2edd6891f12e
Sean Shookman <sms112788@gmail.com>
2016-08-27 10:28:57 -0500

    Adding test for coverage

---
+++ src/test/java/golem/mud/hub/util/SocketUtilTest.java
@@ -0,0 +1,12 @@
+package golem.mud.hub.util;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+public class SocketUtilTest {
+
+    @Test
+    public void testInstantiate() {
+        assertNotNull(new SocketUtil());
+    }
+}
