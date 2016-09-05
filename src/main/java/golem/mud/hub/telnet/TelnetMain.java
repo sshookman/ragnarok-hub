b9c866a3bf0e26ffe06b4c060f078b42d681af01
Sean Shookman <sms112788@gmail.com>
2016-09-05 13:10:51 -0500

    Adds more handle response to telnet main

--- src/main/java/golem/mud/hub/telnet/TelnetMain.java
+++ src/main/java/golem/mud/hub/telnet/TelnetMain.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import golem.mud.hub.das.model.PlayerDO;
 import golem.mud.hub.command.HubCommandFactory;
 import golem.mud.common.exception.CommandException;
+import golem.mud.common.command.CommandResponse;
 
 public class TelnetMain {
 
@@ -17,9 +18,10 @@ public class TelnetMain {
             return;
         }
 
-        renderer.write("================================\n");
-        renderer.write("Welcome to the Dragonfly Library\n");
-        renderer.write("================================\n\n");
+        renderer.write("Welcome to\n");
+        renderer.write("===================================\n");
+        renderer.write("| The Dragonfly IF Telnet Library |\n");
+        renderer.write("===================================\n\n");
 
         HubCommandFactory commandFactory = new HubCommandFactory(session);
         String commandString = "";
@@ -30,12 +32,25 @@ public class TelnetMain {
             commandString = renderer.read();
             
             try {
-                commandFactory.buildCommand(commandString).execute();
+                CommandResponse response = commandFactory.buildCommand(commandString).execute();
+                handleResponse(renderer, response);
             } catch (CommandException exception) {
                 renderer.write(exception.getMessage());
             }
 
             renderer.endl(2);
+        }
+    }
+
+    private static void handleResponse(TelnetRenderer renderer, CommandResponse response) throws IOException {
+        if (response != null && response.getData() != null) {
+            renderer.write("Do you wish to play ");
+            renderer.write(response.getStringData());
+            renderer.write("? (y/n):");
+            String play = renderer.read();
+            if ("Y".equalsIgnoreCase(play) || "YES".equalsIgnoreCase(play)) {
+                renderer.write("START GAME");
+            }
         }
     }
 }
