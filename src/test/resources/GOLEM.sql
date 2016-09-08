7055de13fcc5e4036eca2c1d02a24fa0dd7e19dc
Sean Shookman <sms112788@gmail.com>
2016-09-07 21:22:21 -0500

    Adds c_pickup to SQL init script

--- src/test/resources/GOLEM.sql
+++ src/test/resources/GOLEM.sql
@@ -1,5 +1,20 @@
-DROP TABLE IF EXISTS "c_display";
 DROP TABLE IF EXISTS "entity";
+DROP TABLE IF EXISTS "c_display";
+DROP TABLE IF EXISTS "c_pickup";
 
-CREATE TABLE "c_display" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "entity_id" INTEGER NOT NULL , "message" TEXT NOT NULL );
-CREATE TABLE "entity" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );
+CREATE TABLE "entity" (
+        "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE
+    );
+
+CREATE TABLE "c_display" (
+        "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE,
+        "entity_id" INTEGER NOT NULL,
+        "message" TEXT NOT NULL
+    );
+
+CREATE TABLE "c_pickup" (
+        "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE,
+        "entity_id" INTEGER NOT NULL,
+        "stackable" BOOLEAN DEFAULT FALSE,
+        "quantity" INTEGER DEFAULT 1
+    );
