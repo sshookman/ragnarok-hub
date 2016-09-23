b69085df55b44fd5f1a05d131b117a7b607cacc9
Sean Shookman <sms112788@gmail.com>
2016-09-22 22:39:08 -0500

    Adds getStartingPoint method

--- src/main/java/golem/mud/story/das/StoryAggregateDataService.java
+++ src/main/java/golem/mud/story/das/StoryAggregateDataService.java
@@ -1,28 +1,44 @@
 package golem.mud.story.das;
 
+import java.util.List;
 import java.sql.Connection;
 
-import golem.mud.story.das.model.DisplayComponentDO;
 import golem.mud.common.das.ConnectionManager;
+import golem.mud.story.das.model.*;
 
 public class StoryAggregateDataService {
 
     public EntityDataService entityService;
+    public EntryComponentDataService entryService;
     public DisplayComponentDataService displayService;
     public PathComponentDataService pathService;
 
     public StoryAggregateDataService(String storyPath) throws Exception {
         Connection story = ConnectionManager.establishConnection(storyPath); 
         this.entityService = new EntityDataService(story);
+        this.entryService = new EntryComponentDataService(story);
         this.displayService = new DisplayComponentDataService(story);
         this.pathService = new PathComponentDataService(story);
     }
 
     public DisplayComponentDO getStartingPoint() {
 
-        //TODO: Need a good way of marking an entity as the starting point
-        //Grab starting point and obtain entityId
-        //Get display data based off given entityId
-        return null;
+        EntryComponentDO entry = new EntryComponentDO();
+        entry.setPosition(1);
+        List<EntryComponentDO> entries = entryService.read(entry.toMap());
+        if (entries != null && !entries.isEmpty()) {
+            entry = entries.get(0);
+        } else {
+            return null;
+        }
+        
+        DisplayComponentDO display = new DisplayComponentDO();
+        display.setEntityId(entry.getEntityId());
+        List<DisplayComponentDO> displays = displayService.read(display.toMap());
+        if (displays != null && !displays.isEmpty()) {
+            return displays.get(0);
+        } else {
+            return null;
+        }
     }
 }
--- src/test/java/golem/mud/story/das/StoryAggregateDataServiceTest.java
+++ src/test/java/golem/mud/story/das/StoryAggregateDataServiceTest.java
@@ -15,10 +15,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import golem.mud.common.das.ConnectionManager;
+import golem.mud.story.das.model.*;
 
 public class StoryAggregateDataServiceTest {
 
     private static final String PATH = "src/test/resources/library/";
+
+    private static final String START_MESSAGE = 
+            "The human race is over. The Checkered flag has dropped.\n"
+            + "The Desolate is rising. The beating heart has stopped.";
 
     private static Connection story;
 	private static StoryAggregateDataService services;
@@ -42,4 +47,26 @@ public class StoryAggregateDataServiceTest {
         assertNotNull(services.displayService);
         assertNotNull(services.pathService);
 	}
+
+    @Test
+    public void testGetStartingPoint() {
+
+        assertTrue(services.entityService.create(new EntityDO()));
+
+        EntryComponentDO entry = new EntryComponentDO();
+        entry.setEntityId(1);
+        entry.setPosition(1);
+        assertTrue(services.entryService.create(entry));
+
+        DisplayComponentDO display = new DisplayComponentDO();
+        display.setEntityId(1);
+        display.setMessage(START_MESSAGE);
+        assertTrue(services.displayService.create(display));
+
+        DisplayComponentDO startingPoint = services.getStartingPoint();
+        assertNotNull(startingPoint);
+        assertNotNull(startingPoint.getRowId());
+        assertEquals(Integer.valueOf(1), startingPoint.getEntityId());
+        assertEquals(START_MESSAGE, startingPoint.getMessage());
+    }
 }
