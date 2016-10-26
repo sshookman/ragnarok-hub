8d91f7ce38665b0b5ce2eb1178e6bd59887f894c
Sean Shookman <sms112788@gmail.com>
2016-10-25 23:40:15 -0500

    hud examples

---
+++ hud-examples.sh
@@ -0,0 +1,115 @@
+echo "
+----------------------------------------------------------------------------------------------------
+The______  _        _______          _________ _______ _________ ______  _________ _______  _       
+ (  ____ \( (    /|(  ____ \|\     /|\__   __/(  ____ )\__   __/(  __  \ \__   __/(  ___  )( (    /|
+ | (    \/|  \  ( || (    \/| )   ( |   ) (   | (    )|   ) (   | (  \  )   ) (   | (   ) ||  \  ( |
+ | (__    |   \ | || |      | (___) |   | |   | (____)|   | |   | |   ) |   | |   | |   | ||   \ | |
+ |  __)   | (\ \) || |      |  ___  |   | |   |     __)   | |   | |   | |   | |   | |   | || (\ \) |
+ | (      | | \   || |      | (   ) |   | |   | (\ (      | |   | |   ) |   | |   | |   | || | \   |
+ | (____/\| )  \  || (____/\| )   ( |___) (___| ) \ \_____) (___| (__/  )___) (___| (___) || )  \  |
+ (_______/|/    )_)(_______/|/     \|\_______/|/   \__/\_______/(______/ \_______/(_______)|/    )_)
+  _       _________ ______   _______  _______  _______          
+ ( \      \__   __/(  ___ \ (  ____ )(  ___  )(  ____ )|\     /|
+ | (         ) (   | (   ) )| (    )|| (   ) || (    )|( \   / )
+ | |         | |   | (__/ / | (____)|| (___) || (____)| \ (_) / 
+ | |         | |   |  __ (  |     __)|  ___  ||     __)  \   /  
+ | |         | |   | (  \ \ | (\ (   | (   ) || (\ (      ) (   
+ | (____/\___) (___| )___) )| ) \ \__| )   ( || ) \ \__   | |   
+ (_______/\_______/|/ \___/ |/   \__/|/     \||/   \__/   \_/   of Interactive Fiction
+----------------------------------------------------------------------------------------------------
+
+    [1] Library
+    [2] Authors
+    [3] Stories
+    [4] Config
+    [5] Help
+
+
+ <<[P]______________________________LIBRARY______________________________[N]>>
+
+ +--------+-------------+------------+----------------------+----------------+
+ |   ID   |    Title    |   Author   |         Tags         |     Rating     |
+ +--------+-------------+------------+----------------------+----------------+
+ | [6]    | Game This   | Joe Bob    | Horror, Mystery      |   ***-- (29)   |
+ | [7]    | Game That   | Moe Bob    | Horror, Action       |   ****- (24)   |
+ | [8]    | Game Other  | Joe Lob    | Horror, Adventure    |   **--- (93)   |
+ | [9]    | Game Also   | Moe Sob    | Fantasy, Mystery     |   ***** (1)    |
+ | [10]   | Game Not    | Joe Job    | Action, Mystery      |   ****- (352)  |
+ +--------+-------------+------------+----------------------+----------------+
+
+                                                                  Page 2 of 26
+
+
+ Game This by Joe Bob                                                ***-- (29)
+ ______________________________________________________________________________
+
+    A dark tale of murder and mayhem. Secrets abound in the shadows of garvinia
+ city. It's up to you, a fresh young detective, to solve them. Do you have what
+ it takes to wrestle with the city that cannot be tamed?
+
+    [0] Back
+    [1] Play
+
+
+ <<[P]__________________AUTHORS_________________[N]>>
+
+ +--------+------------+-----------+----------------+
+ |   ID   |    Name    |  Stories  |     Rating     |
+ +--------+------------+-----------+----------------+
+ | [6]    | Joe Bob    | 12        |   ***-- (43)   |
+ | [7]    | Moe Bob    | 9         |   ****- (65)   |
+ | [8]    | Joe Lob    | 38        |   **--- (77)   |
+ | [9]    | Moe Sob    | 52        |   ***** (23)   |
+ | [10]   | Joe Job    | 2         |   *---- (14)   |
+ +--------+------------+-----------+----------------+
+
+                                          Page 2 of 6
+
+
+ Joe Bob                                                             ***-- (43)
+ ______________________________________________________________________________
+
+    Dabbling primarily in dark mysteries, I prefer to keep the reader guessing
+ in my stories. You'll never guess what happens next, but if you're clever
+ enough, at least you'll live to see it!
+
+    [0] Back
+    [1] Collection
+
+
+ <<[P]_____________STORIES_____________[N]>>
+
+ +--------+-------------+------------------+
+ |   ID   |    Title    |     Readers      |
+ +--------+-------------+------------------+
+ | [2]    | Game Woot   | Sammantha, Gary  |
+ | [1]    | Game Boot   | Hercules, Bob    |
+ | [33]   | Game Shoot  | Jon, Joe, Greg   |
+ | [4]    | Game Loot   | Gary, Chris      |
+ | [23]   | Game Fart   | Sean             |
+ +--------+-------------+------------------+
+
+                                 Page 2 of 3
+
+
+ Game Woot by Joe Bob                                                ***-- (29)
+ ______________________________________________________________________________
+
+    A pale ale of murder and mayhem. Secrets abound in the shadows of garvinia
+ city. It's up to you, a fresh young detective, to solve them. Do you have what
+ it takes to wrestle with the city that cannot be tamed?
+
+    [0] Back
+    [1] Play as Samantha
+    [2] Play as Gary
+
+
+
+________________________CONFIG
+
+    [0] Back
+    [1] Text Color:         <RED>
+    [2] Show Highlights:     <ON>
+    [3] Print Speed:       <FAST>
+    [4] Print Sound: <TYPEWRITER>
+"
