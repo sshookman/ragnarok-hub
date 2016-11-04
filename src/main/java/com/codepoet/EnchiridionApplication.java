8e6ce92894f58a369ee308d07a00c568bdd36265
Sean Shookman <sms112788@gmail.com>
2016-11-03 22:55:30 -0500

    So... it begins...

---
+++ src/main/java/com/codepoet/EnchiridionApplication.java
@@ -0,0 +1,12 @@
+package com.codepoet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchiridionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchiridionApplication.class, args);
+	}
+}
