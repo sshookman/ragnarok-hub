cf5cac07ed07245ba3076559d2ec1eac7f69c0ba
Sean Shookman <sms112788@gmail.com>
2016-11-12 22:20:30 -0600

    + session test

---
+++ src/test/java/com/codepoet/enchiridion/server/SessionTest.java
@@ -0,0 +1,41 @@
+package com.codepoet.enchiridion.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.Socket;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SessionTest {
+
+	private static Socket socketMock;
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		socketMock = mock(Socket.class);
+
+		when(socketMock.getInputStream()).thenReturn(new ByteArrayInputStream("INPUT".getBytes()));
+		when(socketMock.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+	}
+
+	@Test
+	public void testSession() {
+		Session session = Session.instance(socketMock);
+
+		assertNotNull(session.getId());
+		assertNotNull(session.getRenderer());
+		assertTrue(session.isOpen());
+
+		session.close();
+	}
+
+	@Test
+	public void testSessionWithNullSocket() {
+		assertNull(Session.instance(null));
+	}
+}
