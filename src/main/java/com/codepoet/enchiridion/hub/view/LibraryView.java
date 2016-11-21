362f25e08802d426656a2199cb2598fe0a257684
Sean Shookman <sms112788@gmail.com>
2016-11-20 22:55:51 -0600

    + Template classes

--- src/main/java/com/codepoet/enchiridion/hub/view/LibraryView.java
+++ src/main/java/com/codepoet/enchiridion/hub/view/LibraryView.java
@@ -8,6 +8,22 @@ import java.util.Map;
 
 public class LibraryView implements View {
 
+	private static final String TABLE_TEMPLATE = ""
+			+ " <<{{P}}______________________________{{NAME}}______________________________{{N}}>>\n"
+			+ "\n"
+			+ " +--------+-------------+------------+----------------------+----------------+\n"
+			+ " |   ID   |    Title    |   Author   |         Tags         |     Rating     |\n"
+			+ " +--------+-------------+------------+----------------------+----------------+\n"
+			+ " | [6]    | Game This   | Joe Bob    | Horror, Mystery      |   ***-- (29)   |\n"
+			+ " | [7]    | Game That   | Moe Bob    | Horror, Action       |   ****- (24)   |\n"
+			+ " | [8]    | Game Other  | Joe Lob    | Horror, Adventure    |   **--- (93)   |\n"
+			+ " | [9]    | Game Also   | Moe Sob    | Fantasy, Mystery     |   ***** (1)    |\n"
+			+ " | [10]   | Game Not    | Joe Job    | Action, Mystery      |   ****- (352)  |\n"
+			+ " +--------+-------------+------------+----------------------+----------------+\n"
+			+ "\n"
+			+ "                                                                  Page 2 of 26\n"
+			+ "";
+
 	@Override
 	public Request render(Renderer renderer, Map<String, Object> model) {
 		List<StoryDO> stories = (List<StoryDO>) model.get("stories");
--- src/main/java/com/codepoet/enchiridion/hub/view/WelcomeView.java
+++ src/main/java/com/codepoet/enchiridion/hub/view/WelcomeView.java
@@ -1,34 +1,15 @@
 package com.codepoet.enchiridion.hub.view;
 
 import com.codepoet.enchiridion.hub.model.Request;
+import com.codepoet.enchiridion.hub.template.TemplateUtil;
+import static com.codepoet.enchiridion.hub.template.TemplateUtil.WELCOME_TEMPLATE;
 import com.codepoet.enchiridion.render.Renderer;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WelcomeView implements View {
-
-	private static final String WELCOME_TEMPLATE = ""
-			+ "----------------------------------------------------------------------------------------------------\n"
-			+ "The______  _        _______          _________ _______ _________ ______  _________ _______  _\n"
-			+ " (  ____ \\( (    /|(  ____ \\|\\     /|\\__   __/(  ____ )\\__   __/(  __  \\ \\__   __/(  ___  )( (    /|\n"
-			+ " | (    \\/|  \\  ( || (    \\/| )   ( |   ) (   | (    )|   ) (   | (  \\  )   ) (   | (   ) ||  \\  ( |\n"
-			+ " | (__    |   \\ | || |      | (___) |   | |   | (____)|   | |   | |   ) |   | |   | |   | ||   \\ | |\n"
-			+ " |  __)   | (\\ \\) || |      |  ___  |   | |   |     __)   | |   | |   | |   | |   | |   | || (\\ \\) |\n"
-			+ " | (      | | \\   || |      | (   ) |   | |   | (\\ (      | |   | |   ) |   | |   | |   | || | \\   |\n"
-			+ " | (____/\\| )  \\  || (____/\\| )   ( |___) (___| ) \\ \\_____) (___| (__/  )___) (___| (___) || )  \\  |\n"
-			+ " (_______/|/    )_)(_______/|/     \\|\\_______/|/   \\__/\\_______/(______/ \\_______/(_______)|/    )_)\n"
-			+ "  _       _________ ______   _______  _______  _______\n"
-			+ " ( \\      \\__   __/(  ___ \\ (  ____ )(  ___  )(  ____ )|\\     /|\n"
-			+ " | (         ) (   | (   ) )| (    )|| (   ) || (    )|( \\   / )\n"
-			+ " | |         | |   | (__/ / | (____)|| (___) || (____)| \\ (_) /\n"
-			+ " | |         | |   |  __ (  |     __)|  ___  ||     __)  \\   /\n"
-			+ " | |         | |   | (  \\ \\ | (\\ (   | (   ) || (\\ (      ) (\n"
-			+ " | (____/\\___) (___| )___) )| ) \\ \\__| )   ( || ) \\ \\__   | |\n"
-			+ " (_______/\\_______/|/ \\___/ |/   \\__/|/     \\||/   \\__/   \\_/   of Interactive Fiction\n"
-			+ "----------------------------------------------------------------------------------------------------\n"
-			+ "\n"
-			+ "{{OPTIONS}}"
-			+ "\n";
 
 	@Override
 	public Request render(final Renderer renderer, Map<String, Object> model) {
@@ -51,30 +32,34 @@ public class WelcomeView implements View {
 	}
 
 	public String buildWelcomeView(Map<String, Object> model) {
-		String welcomeView = WELCOME_TEMPLATE;
 		String options = "";
 
 		if (model != null && model.containsKey("options")) {
 			Object optionsObject = model.get("options");
 			if (optionsObject instanceof List) {
-				options = buildOptionsList(optionsObject);
+				options = buildOptionsList("", (List<String>) optionsObject);
 			}
 		}
 
-		return welcomeView.replace("{{OPTIONS}}", options);
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("options", options);
+
+		return TemplateUtil.compose(WELCOME_TEMPLATE, data);
 	}
 
-	private String buildOptionsList(Object optionsObject) {
-		StringBuilder optionsBuilder = new StringBuilder();
-		List<String> optionList = (List<String>) optionsObject;
+	private String buildOptionsList(final String options, final List<String> optionList) {
 
-		int index = 1;
-		for (String option : optionList) {
-			optionsBuilder
-					.append("    [").append(index++).append("] ")
-					.append(option).append("\n");
+		if (optionList.isEmpty()) {
+			return options;
 		}
 
-		return optionsBuilder.toString();
+		StringBuilder optionsBuilder = new StringBuilder()
+				.append("    [").append(optionList.size()).append("] ")
+				.append(optionList.get(optionList.size() - 1)).append("\n")
+				.append(options);
+
+		List<String> reducedOptionList = new ArrayList<>(optionList.subList(0, optionList.size() - 1));
+
+		return buildOptionsList(optionsBuilder.toString(), reducedOptionList);
 	}
 }
