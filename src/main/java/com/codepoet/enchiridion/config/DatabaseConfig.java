c6a29d4ff5ac28de91f7d6e3db537479cdb524be
Sean Shookman <sms112788@gmail.com>
2016-11-25 00:01:35 -0600

    + application.yml

--- src/main/java/com/codepoet/enchiridion/config/DatabaseConfig.java
+++ src/main/java/com/codepoet/enchiridion/config/DatabaseConfig.java
@@ -2,14 +2,18 @@ package com.codepoet.enchiridion.config;
 
 import com.codepoet.enchiridion.das.ConnectionManager;
 import java.sql.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DatabaseConfig {
 
+	@Value("database.hub")
+	private String hubDatabase;
+
 	@Bean
 	public Connection hubDatabaseConnection() throws Exception {
-		return ConnectionManager.establishConnection("srv/hub.sqlite");
+		return ConnectionManager.establishConnection(hubDatabase);
 	}
 }
