bbf9ebc40968afa2fc71c2bcb9f3f890167819f3
Sean Shookman <sms112788@gmail.com>
2017-01-29 23:58:28 -0600

    Adds test for DataService

--- src/main/java/com/codepoet/enchiridion/das/DataService.java
+++ src/main/java/com/codepoet/enchiridion/das/DataService.java
@@ -7,35 +7,36 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DataService<D extends AbstractDO> {
 
-	private final Logger LOGGER = Logger.getLogger(DataService.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(DataService.class.getName());
 
-	private Connection connection;
-	private D dataObjectClass;
+	private final Connection connection;
+	private final D dataObjectClass;
 
 	public DataService(final D dataObject, final Connection connection) {
 		this.dataObjectClass = dataObject;
 		this.connection = connection;
 	}
 
-	private boolean executeUpdate(final String query) {
+	private Boolean executeUpdate(final String query) {
 		try {
 			Statement statement = connection.createStatement();
 			statement.executeUpdate(query);
 			statement.close();
 			return true;
 		} catch (Exception exception) {
-			LOGGER.severe("Failed to execute query: " + query);
-			LOGGER.severe(exception.getMessage());
+			LOGGER.log(Level.SEVERE, "Failed to execute query: {0}", query);
+			LOGGER.log(Level.SEVERE, exception.getMessage());
 			return false;
 		}
 	}
 
 	private List<D> executeQuery(final String query) {
-		List<D> dataObjects = new ArrayList<D>();
+		List<D> dataObjects = new ArrayList<>();
 		try {
 			Statement statement = connection.createStatement();
 			ResultSet results = statement.executeQuery(query);
@@ -47,16 +48,21 @@ public class DataService<D extends AbstractDO> {
 			results.close();
 			statement.close();
 		} catch (Exception exception) {
-			LOGGER.severe("Failed to execute query: " + query);
-			LOGGER.severe(exception.getMessage());
+			LOGGER.log(Level.SEVERE, "Failed to execute query: {0}", query);
+			LOGGER.log(Level.SEVERE, exception.getMessage());
 		}
 		return dataObjects;
 	}
 
 	public List<D> read(Map<String, String> search) {
+		if (search == null) {
+			return null;
+		}
+
 		String query = new QueryBuilder.SelectQuery(dataObjectClass.getTable())
 				.whereEquals(search)
 				.build();
+
 		return executeQuery(query);
 	}
 
@@ -64,6 +70,7 @@ public class DataService<D extends AbstractDO> {
 		if (rowId == null) {
 			return null;
 		}
+
 		String idString = rowId.toString();
 		String query = new QueryBuilder.SelectQuery(dataObjectClass.getTable())
 				.whereEquals("id", idString)
@@ -73,9 +80,9 @@ public class DataService<D extends AbstractDO> {
 		return results.isEmpty() ? null : results.get(0);
 	}
 
-	public boolean create(AbstractDO dataObject) {
+	public Boolean create(AbstractDO dataObject) {
 		if (dataObject == null) {
-			return false;
+			return null;
 		}
 
 		String query = new QueryBuilder.InsertQuery(dataObjectClass.getTable())
@@ -85,9 +92,9 @@ public class DataService<D extends AbstractDO> {
 		return executeUpdate(query);
 	}
 
-	public boolean update(final Integer rowId, final AbstractDO dataObject) {
+	public Boolean update(final Integer rowId, final AbstractDO dataObject) {
 		if (rowId == null || dataObject == null) {
-			return false;
+			return null;
 		}
 
 		String idString = rowId.toString();
@@ -99,10 +106,11 @@ public class DataService<D extends AbstractDO> {
 		return executeUpdate(query);
 	}
 
-	public boolean delete(final Integer rowId) {
+	public Boolean delete(final Integer rowId) {
 		if (rowId == null) {
-			return false;
+			return null;
 		}
+
 		String idString = rowId.toString();
 		String query = new QueryBuilder.DeleteQuery(dataObjectClass.getTable())
 				.whereEquals("id", idString)
---
+++ src/test/java/com/codepoet/enchiridion/das/DataServiceTest.java
@@ -0,0 +1,111 @@
+package com.codepoet.enchiridion.das;
+
+import com.codepoet.enchiridion.das.model.StoryDO;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataServiceTest {
+
+	private static final String PATH = "src/test/resources/library/DataServiceTest.gmh";
+	private Connection connection;
+	private DataService<StoryDO> testDataService;
+
+	@AfterClass
+	public static void teardown() {
+		File file = new File(PATH);
+		file.delete();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		connection = ConnectionManager.establishConnection(PATH);
+		testDataService = new DataService(new StoryDO(), connection);
+	}
+
+	@Test
+	public void testErrors() {
+		Integer noInt = null;
+		Map<String, String> noMap = null;
+		assertNull(testDataService.read(noInt));
+		assertNull(testDataService.read(noMap));
+		assertNull(testDataService.create(null));
+		assertNull(testDataService.update(null, new StoryDO()));
+		assertNull(testDataService.update(1, null));
+		assertNull(testDataService.delete(null));
+		assertTrue(testDataService.read(new HashMap<>()).isEmpty());
+		assertFalse(testDataService.create(new StoryDO()));
+	}
+
+	@Test
+	public void testCRUD() throws FileNotFoundException, SQLException {
+		ConnectionManager.initGolemMudHub(connection);
+		testRead_Map_Empty();
+		testCreate();
+		testRead_Integer();
+		testUpdate();
+		testRead_Map();
+		testDelete();
+		testRead_Map_Empty();
+
+	}
+
+	public void testRead_Map_Empty() {
+		List<StoryDO> stories = testDataService.read(new HashMap<>());
+		assertNotNull(stories);
+		assertTrue(stories.isEmpty());
+	}
+
+	public void testRead_Map() {
+		List<StoryDO> stories = testDataService.read(new HashMap<>());
+		assertNotNull(stories);
+		assertEquals(1, stories.size());
+
+		StoryDO story = stories.get(0);
+		assertNotNull(story);
+		assertEquals(Integer.valueOf(1), story.getRowId());
+		assertEquals("MyStoryUpdated", story.getName());
+		assertEquals("/there", story.getPath());
+	}
+
+	public void testRead_Integer() {
+		StoryDO story = (StoryDO) testDataService.read(1);
+		assertNotNull(story);
+		assertEquals(Integer.valueOf(1), story.getRowId());
+		assertEquals("MyStory", story.getName());
+		assertEquals("/here", story.getPath());
+
+		assertNull(testDataService.read(2));
+	}
+
+	public void testCreate() {
+		StoryDO story = new StoryDO();
+		story.setName("MyStory");
+		story.setPath("/here");
+		assertTrue(testDataService.create(story));
+	}
+
+	public void testUpdate() {
+		StoryDO story = new StoryDO();
+		story.setName("MyStoryUpdated");
+		story.setPath("/there");
+		assertTrue(testDataService.update(1, story));
+	}
+
+	public void testDelete() {
+		assertTrue(testDataService.delete(1));
+	}
+
+}
