cf1f15c460ce92013efde4a839c1a18d3e74bb12
Sean Shookman <sms112788@gmail.com>
2018-10-01 10:48:02 -0500

    Adds javadocs for clarity and ease of use (#3)
    * Adds javadocs for clarity and ease of use
    
    * Fixes bug with exception dec that was not needed

--- src/main/java/codepoet/ragnarok/Application.java
+++ src/main/java/codepoet/ragnarok/Application.java
@@ -3,9 +3,13 @@ package codepoet.ragnarok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Ragnarok Hub.
+ *
+ * A Telnet accessible hub that serves out a library of text-based games.
+ */
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/codepoet/ragnarok/server/Server.java
+++ src/main/java/codepoet/ragnarok/server/Server.java
@@ -8,6 +8,12 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * The Server stands as the starting point for the hub.
+ *
+ * The server maitains the SessionManager and the ClientManager and also
+ * supplies the main loop that listens for incoming connections over telnet.
+ */
 @Component
 public class Server {
 
@@ -18,32 +24,40 @@ public class Server {
 	private final ClientManager clientManager;
 	private final ServerSocket server;
 
+    /**
+     * Constructor for Server - Autowired Managers.
+     *
+     * The constructor autowires in the SessionManager and ClientManager components to
+     * allow for new sessions and clients to be created in the listen loop which is
+     * started after the other compenents have been wired in.
+     */
 	@Autowired
 	public Server(final SessionManager sessionManager, final ClientManager clientManager) throws IOException {
 		this.sessionManager = sessionManager;
 		this.clientManager = clientManager;
 		this.server = new ServerSocket(PORT);
-		start();
+		listen();
 	}
 
-	private void start() {
+    /**
+     * Listens for incoming connections on the specified PORT number.
+     * 
+     * Each connection is accepted and creates a socket which is stored into
+     * a session instance inside the SessionManager. A client is then launched
+     * from this session where the user interaction will take place.
+     */
+	private void listen() {
 		try {
 			LOGGER.log(Level.INFO, "Server Listening on Port : {0}", PORT);
-			listen();
+            while (true) {
+                Socket socket = server.accept();
+                Session session = sessionManager.instance(socket);
+                clientManager.launch(session);
+                sessionManager.removeClosedSessions();
+            }
 		} catch (Exception exception) {
 			LOGGER.log(Level.WARNING, "Shutting Down Due To {0}", exception.getMessage());
 			clientManager.shutdown();
-		}
-	}
-
-	private void listen() throws Exception {
-		while (true) {
-			Socket socket = server.accept();
-			Session session = sessionManager.instance(socket);
-
-			LOGGER.log(Level.INFO, "Opening Session {0}", session.getId());
-			clientManager.launch(session);
-			sessionManager.removeClosedSessions();
 		}
 	}
 }
